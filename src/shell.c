f805765017f6522e501782eed50f3aff30346c73
foldedtoad <foldedtoad@gmail.com>
2023-06-04 23:05:17 -0700

    more shell work

--- src/shell.c
+++ src/shell.c
@@ -12,6 +12,7 @@
 #include <zephyr/devicetree.h>
 
 #include "shell.h"
+#include "keyboard.h" 
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(shell, 3);
@@ -24,7 +25,33 @@ static int cmd_shell_info(const struct shell *sh, size_t argc, char *argv[])
     ARG_UNUSED(argc);
     ARG_UNUSED(argv);
 
-    LOG_INF("** Info ***");
+    shell_print(sh, "** Welcome to Caliper Keyboard ***");
+
+    return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+static int cmd_shell_line_end(const struct shell *sh, size_t argc, char *argv[])
+{
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+
+    static bool line_end = false;
+    char * setting;
+
+    // toggle state
+    if (line_end) {
+        line_end = false;
+        setting = "\\0";
+    }
+    else {
+        line_end = true;
+        setting = "\\n\\0";
+    }
+
+    shell_print(sh, "newline:  %s", setting);
 
     return 0;
 }
@@ -37,7 +64,11 @@ static int cmd_shell_test(const struct shell *sh, size_t argc, char *argv[])
     ARG_UNUSED(argc);
     ARG_UNUSED(argv);
 
-    LOG_INF("** Test ***");
+    char * string = (char*) argv[1];
+
+    shell_print(sh, "test: %s", string);
+
+    keyboard_send_string(string);
 
     return 0;
 }
@@ -46,14 +77,14 @@ static int cmd_shell_test(const struct shell *sh, size_t argc, char *argv[])
 /*                                                                           */
 /*---------------------------------------------------------------------------*/
 
-SHELL_STATIC_SUBCMD_SET_CREATE(caliper_commands,
-    SHELL_CMD(test, NULL, "Caliper Test\n", cmd_shell_test),
-    SHELL_CMD(info, NULL, "Caliper Info\n", cmd_shell_info),
+SHELL_STATIC_SUBCMD_SET_CREATE(caliper_cmds,
+    SHELL_CMD_ARG(test, NULL, "caliper test <string>", cmd_shell_test, 2, 0),
+    SHELL_CMD(line_end, NULL, "caliper line_end (toggle)", cmd_shell_line_end),
+    SHELL_CMD(info, NULL, "caliper info", cmd_shell_info),
     SHELL_SUBCMD_SET_END
 );
 
-SHELL_CMD_REGISTER(caliper, &caliper_commands,
-           "Caliper commands", NULL);
+SHELL_CMD_REGISTER(caliper, &caliper_cmds, "Caliper commands", NULL);
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
