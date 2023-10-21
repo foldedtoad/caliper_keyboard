c2d41065198a6568801e744cd540e2f799ef906f
foldedtoad <foldedtoad@gmail.com>
2023-10-20 22:12:35 -0700

    start of buzzer support

--- inc/caliper_gpio.h
+++ inc/caliper_gpio.h
@@ -24,10 +24,22 @@
 #define DATA_FLAGS      DT_PHA_BY_IDX(DT_ALIAS(caliper_data), gpios, 0, flags)
 #define DATA_LABEL      DT_PROP(DT_ALIAS(caliper_data), label)
 
+/*
+ *  Buzzer gpio pin is same as Debug gpio pin.
+ *  Configuring of Buzzer and Debug are mutually exclusive.
+ */
+#if 0
 #define DEBUG_NODE      DT_ALIAS(caliper_debug)
 #define DEBUG           DT_GPIO_PIN(DT_ALIAS(caliper_debug), gpios)
 #define DEBUG_PORT      DT_LABEL(DT_PHANDLE_BY_IDX(DT_ALIAS(caliper_debug), gpios, 0))
 #define DEBUG_FLAGS     DT_PHA_BY_IDX(DT_ALIAS(caliper_debug), gpios, 0, flags)
 #define DEBUG_LABEL     DT_PROP(DT_ALIAS(caliper_debug), label)
+#else
+#define BUZZER_NODE     DT_ALIAS(caliper_debug)
+#define BUZZER          DT_GPIO_PIN(DT_ALIAS(caliper_debug), gpios)
+#define BUZZER_PORT     DT_LABEL(DT_PHANDLE_BY_IDX(DT_ALIAS(caliper_debug), gpios, 0))
+#define BUZZER_FLAGS    DT_PHA_BY_IDX(DT_ALIAS(caliper_debug), gpios, 0, flags)
+#define BUZZER_LABEL    DT_PROP(DT_ALIAS(caliper_debug), label)
+#endif
 
 #endif  /* __CALIPER_GPIO_H */
---
+++ src/buzzer.c
@@ -0,0 +1,168 @@
+/*
+ *  buzzer.c  -- buzzer driver
+ *
+ *  Copyright (c) 2023   Callender-Consulting
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/drivers/pwm.h> 
+#include <inttypes.h>
+
+#include "events.h"
+#include "buzzer.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(buzzer, LOG_LEVEL_INF);
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+static const struct pwm_dt_spec pwm_spec = 
+                        PWM_DT_SPEC_GET(DT_NODELABEL(pwm_buzzer0));
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+static void buzzer_timeout_callback(struct k_timer *timer);
+
+K_TIMER_DEFINE(buzzer_timer, buzzer_timeout_callback, NULL);
+
+static bool  in_play = false;
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+static void set_frequency_and_duty_cycle(uint32_t frequency, 
+                                         uint32_t duty_cycle_percent)
+{
+//    nrf_pwm_set_max_value((HFCLKHZ + (frequency / 2)) / frequency);
+//    nrf_pwm_set_value(0, (HFCLKHZ / frequency) * duty_cycle_percent / 100);
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+void buzzer_set_pwm_parms(void)
+{
+   
+//    nrf_pwm_config_t pwm_config = PWM_DEFAULT_CONFIG;
+
+    uint32_t frequency  = 2700;
+    uint32_t duty_cycle = 50;
+
+#if 0 
+    pwm_config.mode          = PWM_MODE_BUZZER;
+    pwm_config.num_channels  = 1;
+    pwm_config.gpio_num[0]   = BUZZER_PIN;
+    nrf_pwm_init(&pwm_config);
+#endif
+
+    set_frequency_and_duty_cycle(frequency, duty_cycle);
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+static void buzzer_process_playlist(buzzer_play_t * playlist)
+{
+//    gpio_pin_set_dt(buzzer_spec, 0);  // In sure buzzer GPIO is off.
+
+    /*
+     *   Single play.
+     */
+    if (playlist == NULL) {
+        return;
+    }
+
+    /*
+     *   Play list.
+     */
+    switch (playlist->action) {
+
+        case BUZZER_PLAY_TONE:
+            buzzer_set_pwm_parms();
+//            nrf_pwm_set_enabled(true);
+
+            k_timer_start(&buzzer_timer, K_MSEC(500), K_NO_WAIT);
+
+//            app_timer_start(m_buzzer_timer_id,
+//                            (playlist->duration * TIMER_DELAY_ONE_MS),
+//                            &playlist[1]);
+            break;
+
+        case BUZZER_PLAY_QUIET:
+            // Disable while quiet: conserve power.
+//            nrf_pwm_set_enabled(false);
+
+            k_timer_start(&buzzer_timer, K_MSEC(500), K_NO_WAIT);
+
+//            app_timer_start(m_buzzer_timer_id,
+//                            (playlist->duration * TIMER_DELAY_ONE_MS),
+//                            &playlist[1]);
+            break;
+
+        case BUZZER_PLAY_DONE:
+        default:
+            // Disable when done: conserve power.
+//            nrf_pwm_set_enabled(false);
+
+            // Done: set BUZZER pin to "output".
+            in_play = false;
+            break;
+    }
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+static void buzzer_timeout_callback(struct k_timer * timer)
+{
+    buzzer_play_t * playlist = (buzzer_play_t*) timer->user_data;
+
+    buzzer_process_playlist(playlist);
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+uint32_t buzzer_play(buzzer_play_t * playlist)
+{
+    if (in_play)
+        return -1;
+    in_play = true;
+
+    buzzer_process_playlist(playlist);
+
+    return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+void buzzer_stop(void)
+{
+    k_timer_stop(&buzzer_timer);
+
+//    nrf_pwm_set_enabled(false);
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+void buzzer_init(void)
+{
+    if (!device_is_ready(pwm_spec.dev)) {
+        LOG_ERR("PWM device %s is not ready", pwm_spec.dev->name);
+        return;
+    }
+
+//    nrf_pwm_set_enabled(false);
+
+}
